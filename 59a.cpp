ef184fa4e0b28093cd443eae0a5144fba1356305
spol-29 <polineni.sriram9@gmail.com>
2023-02-18 16:23:23 -0600

    59a

--- 59a.cpp
+++ 59a.cpp
@@ -21,7 +21,7 @@ int main(){
         for(int i = 0; i < s.length(); i++){
             if(s[i] >= 65 && s[i] <= 90){
                 s[i] += 32;
-            }
+           }
         }
     }
     cout << s << endl;
