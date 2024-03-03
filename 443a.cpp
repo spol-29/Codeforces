0af2ec02fa4481aef87e6d93a9e14e723e0e2766
spol29 <68199583+spol-29@users.noreply.github.com>
2024-03-03 09:22:30 -0600

    code

--- 443a.cpp
+++ 443a.cpp
@@ -1,5 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+ 
 int main(){
-    
+    string s;
+    getline(cin, s);
+    s.erase(remove(s.begin(), s.end(), '{'), s.end());
+    s.erase(remove(s.begin(), s.end(), '}'), s.end());
+    s.erase(remove(s.begin(), s.end(), ','), s.end());
+    s.erase(remove(s.begin(), s.end(), ' '), s.end());
+    set<char> value;
+    for(int i = 0; i < s.length(); i++){
+        value.insert(s[i]);
+    }
+    cout << value.size();
 }
